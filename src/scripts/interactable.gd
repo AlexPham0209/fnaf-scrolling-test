8764dfbfa910da890c19a489010a1ba13ade974c
AlexPham0209 <121077648+AlexPham0209@users.noreply.github.com>
2025-05-15 12:02:25 -0400

    Added pressed signal to interactable node

--- src/scripts/interactable.gd
+++ src/scripts/interactable.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
 @onready var sprite: Sprite2D = $Icon
+
 var collided = false
+signal pressed
 
 func _on_area_entered(area: Area2D) -> void:
 	sprite.modulate = Color(0,1,0) 
@@ -15,4 +17,4 @@ func _input(event: InputEvent) -> void:
 	if event is not InputEventMouseButton or not event.is_pressed() or not collided:
 		return
 	
-	print("pressed")
+	pressed.emit()
--- src/scripts/scrolling.gd
+++ src/scripts/scrolling.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+
 func _init() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
@@ -9,3 +10,9 @@ func _input(event: InputEvent) -> void:
 		
 	get_tree().paused = not get_tree().paused
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE if get_tree().paused else Input.MOUSE_MODE_CAPTURED
+
+func closed_door() -> void:
+	print("closed door")
+
+func open_curtains() -> void:
+	print("open curtains")
