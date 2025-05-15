5f58e549aa173aa6c9fe9566ecd46f0e03651feb
AlexPham0209 <121077648+AlexPham0209@users.noreply.github.com>
2025-05-15 01:33:11 -0400

    Added screen wrap option to perspective object

--- src/scripts/perspective.gd
+++ src/scripts/perspective.gd
@@ -4,17 +4,19 @@ extends ColorRect
 @export var maxLatitude: float = 180
 @export var center: Vector2 = Vector2(0.5, 0.5)
 @export var fov: Vector2 = Vector2(0.7, 0.75)
+
+@export var x_bound: Vector2 = Vector2(0.0, 1.0)
+@export var y_bound: Vector2 = Vector2(0.0, 1.0)
+@export var mouse_sensitivity: Vector2 = Vector2(0.001, 0.002)
+
 @export var wrap: bool = false
-@export var captured = false
 
 func _ready() -> void:
 	self.visible = true
-
-	if captured:
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
 	maxLongitude = maxLongitude * PI/360
 	maxLatitude = maxLatitude * PI/360
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
 	self.material.set_shader_parameter('centerPoint', center)
 	self.material.set_shader_parameter('maxLongitude', maxLongitude)
@@ -23,20 +25,22 @@ func _ready() -> void:
 	
 
 func _physics_process(delta: float) -> void:	
-	if not captured:
-		var move = get_global_mouse_position() / self.size
-		center.x = fposmod(move.x, 1.0)
-		center.y = clamp(move.y, 0., 1)
+	#if not captured:
+		#var move = get_global_mouse_position() / self.size
+		#center.x = fposmod(move.x, 1.0)
+		#center.y = clamp(move.y, 0., 1)
 	
 	self.material.set_shader_parameter('centerPoint', center)
-	
 	if Input.is_action_just_pressed("ui_accept"):
 		self.visible = not self.visible
 
 func _input(event):
-	if not captured:
+	if Input.get_mouse_mode() != Input.MOUSE_MODE_CAPTURED or event is not InputEventMouseMotion:
 		return
-		
-	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		center.x = fposmod(center.x + event.relative.x * (0.001 / 2), 1.0)
-		center.y = clamp(center.y + event.relative.y * (0.002 / 2), 0, 1)
+	
+	var x = center.x + event.relative.x * mouse_sensitivity.x
+	x = fposmod(x, 1.0) if wrap else x
+	
+	var y = center.y + event.relative.y * mouse_sensitivity.y
+	center.x = clamp(x, x_bound.x, x_bound.y)
+	center.y = clamp(y, y_bound.x, y_bound.y)
